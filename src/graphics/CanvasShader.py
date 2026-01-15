d0519e07f9b633abff654ef64431eb4341182b03
JulianKonowalski <julian.konwalski@gmail.com>
2026-01-15 08:43:21 +0100

    remove .vert shader file

--- src/graphics/CanvasShader.py
+++ src/graphics/CanvasShader.py
@@ -2,6 +2,42 @@ import pathlib
 
 from PySide6.QtOpenGL import QOpenGLShader, QOpenGLShaderProgram
 
+VERT_SOURCE: str = """
+#version 330 core
+
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 tex_coord;
+
+uniform int     u_frame;
+uniform float   u_time;
+uniform float   u_time_delta;
+uniform vec2    u_mouse;
+uniform vec2    u_resolution;
+
+out int     i_frame;
+out float   i_time;
+out float   i_time_delta;
+out vec2    i_mouse;
+out vec2    i_resolution;
+out vec3    i_normal;
+out vec3    i_position;
+out vec2    i_tex_coord;
+
+void main() {
+    gl_Position = vec4(pos, 1.0);
+
+    i_frame         = u_frame;
+    i_time          = u_time;
+    i_time_delta    = u_time_delta;
+    i_mouse         = u_mouse;
+    i_resolution    = u_resolution;
+    i_position      = gl_Position.xyz;
+    i_normal        = normal;
+    i_tex_coord     = tex_coord;
+}
+"""
+
 class CanvasShader(QOpenGLShaderProgram):
     """
     A class representing the shader program.
@@ -28,11 +64,16 @@ class CanvasShader(QOpenGLShaderProgram):
         :type fragment_source: str
         """
         super().__init__()
-        
-        if not self.addShaderFromSourceFile(
+
+        if not self.addShaderFromSourceCode(
             QOpenGLShader.ShaderTypeBit.Vertex,
-            str(pathlib.Path.joinpath(pathlib.Path(__file__).parent.resolve(), "passthrough.vert"))
+            VERT_SOURCE
         ): raise RuntimeError(self.log())
+
+        # if not self.addShaderFromSourceFile(
+        #     QOpenGLShader.ShaderTypeBit.Vertex,
+        #     str(pathlib.Path.joinpath(pathlib.Path(__file__).parent.resolve(), "passthrough.vert"))
+        # ): raise RuntimeError(self.log())
 
         if not self.addShaderFromSourceCode(
             QOpenGLShader.ShaderTypeBit.Fragment,
